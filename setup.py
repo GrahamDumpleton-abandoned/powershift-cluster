b9ccd3fff0de7ac7cd1a6a260bc713f54d518785
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2017-01-02 09:16:01 +1100

    Increment version to 1.0.6.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ classifiers = [
 
 setup_kwargs = dict(
     name='powershift-cluster',
-    version='1.0.5',
+    version='1.0.6',
     description='PowerShift command plugin for creating OpenShift clusters.',
     long_description=long_description,
     url='https://github.com/getwarped/powershift-cluster',
