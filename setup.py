73c5ec2b477dc172dedf0b30dae16665babb14f6
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2017-02-18 20:31:36 +1100

    Update minimum version of cli package.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup_kwargs = dict(
     packages=['powershift', 'powershift.cluster'],
     package_dir={'powershift': 'src/powershift'},
     install_requires=['passlib'],
-    extras_require={'cli': ['powershift-cli>=1.1.1']},
+    extras_require={'cli': ['powershift-cli>=1.1.8']},
     entry_points = {'powershift_cli_plugins': ['cluster = powershift.cluster']},
 )
 
